6716a176306e5bba4a62e670a8214f00b5affbd2
WerbssonSaymon <saymonluz18@gmail.com>
2023-04-21 20:25:26 -0300

    tag  script

--- index.php
+++ index.php
@@ -6,11 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FARMAGLOBAL </title>
     <link rel="stylesheet" type="text/css" href="./styles/bootstrap/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="./styles/style.css">
-    <script type="text/javascript" src="./scripts/bootstrap/bootstrap.js"></script>
-    <script type="text/javascript" src="./scripts/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="./styles/style.css"> 
 </head>
 <body>
-    <h1>titulo</h1>
+    <div>
+        <h1>FARMAGLOBAL <br> A FARMACIA DO BRASIL</h1>
+        <p>Essa farmácia oferece atendimento personalizado<br> e uma grande
+            variedade de produtos, desde vitaminas<br> e suplementos até produtos
+            de beleza e cuidados pessoais.<br> Além disso, oferece serviços como
+            testes de pressão arterial e glicemia,<br> vacinações e serviços farmacêuticos
+            clínicos.<br> Comprometidos com a saúde dos clientes,<br> eles fornecem o
+            melhor atendimento e produtos<br> para uma vida saudável.</p>
+    </div>
+    <button class="btn btn-primary" type="button">ACESSAR</button>
+    <script type="text/javascript" src="./scripts/bootstrap/bootstrap.js"></script>
+    <script type="text/javascript" src="./scripts/script.js"></script>
 </body>
 </html>
