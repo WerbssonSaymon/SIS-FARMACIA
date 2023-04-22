a994fb69bc3ccee90509d0deafb48a102cb933a9
WerbssonSaymon <saymonluz18@gmail.com>
2023-04-21 21:44:54 -0300

    index.php finalizada

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FARMAGLOBAL </title>
-    <link rel="stylesheet" type="text/css" href="./styles/bootstrap/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="./styles/style.css"> 
-</head>
-<body>
-    <div>
-        <h1>FARMAGLOBAL <br> A FARMACIA DO BRASIL</h1>
-        <p>Essa farmácia oferece atendimento personalizado<br> e uma grande
-            variedade de produtos, desde vitaminas<br> e suplementos até produtos
-            de beleza e cuidados pessoais.<br> Além disso, oferece serviços como
-            testes de pressão arterial e glicemia,<br> vacinações e serviços farmacêuticos
-            clínicos.<br> Comprometidos com a saúde dos clientes,<br> eles fornecem o
-            melhor atendimento e produtos<br> para uma vida saudável.</p>
-    </div>
-    <button class="btn btn-primary" type="button">ACESSAR</button>
-    <script type="text/javascript" src="./scripts/bootstrap/bootstrap.js"></script>
-    <script type="text/javascript" src="./scripts/script.js"></script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FARMAGLOBAL</title>
+        <link rel="stylesheet" type="text/css" href="./styles/bootstrap/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="./styles/modelo.css">
+    </head>
+    <body>
+        <section class="container d-flex flex-column justify-content-center align-items-center">
+            <h1 class="title text-center">FARMAGLOBAL <br> A FARMACIA DO BRASIL</h1>
+            <button class="btn btn-primary shadow-sm mb-4" type="button"><strong>ACESSAR</strong></button>
+        </section>
+        <!-- scripts -->
+        <script type="text/javascript" src="./scripts/bootstrap/bootstrap.js"></script>
+        <script type="text/javascript" src="./scripts/script.js"></script>
+    </body>
 </html>
